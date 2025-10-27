4c61fd3d079e4868d7cb800c2824077986e13717
Francis <92519881+francisokeagu@users.noreply.github.com>
2025-10-27 16:05:49 -0400

    Update main_function_library_.py

--- src/main_function_library_.py
+++ src/main_function_library_.py
@@ -386,5 +386,29 @@ def search_books(
 
     return {"total": len(scored), "results": all_results, "page": 1, "page_size": None}
 
+#Circulation manager
+from datetime import date, timedelta
 
+class CirculationManager:
+    def __init__(self):
+        self.is_available = True
+        self.due_date = None
 
+    def get_due_date(self):
+        return self.due_date
+
+    def is_available(self):
+        return self.is_available
+
+    def checkout_book(self, loan_days):
+        if self.is_available:
+            self.is_available = False
+            self.due_date = date.today() + timedelta(days=loan_days)
+            return True
+        else:
+            return False
+
+    def return_book(self):
+        if not self.is_available:
+            self.is_available = True
+            self.due_date = None
